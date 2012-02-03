3c36fa38b593be0655429927ecad5849a6ca7d28
Maxime Bourget <bmx007@gmail.com>
2012-02-03 17:03:43 +0000

    Add Ordering.

---
+++ lib/lims-core/ordering/order.rb
@@ -0,0 +1,11 @@
+# vi: ts=2:sts=2:et:sw=2 spell:spelllang=en 
+require 'common'
+
+module Lims::Core
+  module Ordering
+    # An order is what a project manager is asking for, typically
+    # some samples to be sequenced in a particular waysa sequence
+    class Order
+    end
+  end
+end
