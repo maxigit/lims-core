63c615d2b431a19e80b63977f28591d72c385a71
Maxime Bourget <bmx007@gmail.com>
2012-02-08 11:30:48 +0000

    Add quota

--- lib/lims-core/funding/project.rb
+++ lib/lims-core/funding/project.rb
@@ -4,7 +4,7 @@ require 'common'
 module Lims::Core
   module Funding
     # A project corresponds to a source of funding.
-    # It has a manager and quotas.
+    # It has a manager and {Quota quotas}.
     class Project
     end
   end
---
+++ lib/lims-core/funding/quota.rb
@@ -0,0 +1,11 @@
+# vi: ts=2:sts=2:et:sw=2 spell:spelllang=en 
+require 'common'
+
+module Lims::Core
+  module Funding
+    # Allowance for a specific type of work per {Project project}.
+    # A quota should belong to a project and relate to something quotable.
+    class Quota
+    end
+  end
+end
