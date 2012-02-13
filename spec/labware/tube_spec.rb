b86819f077c3833dc7456a6e8b24b39d50016843
Maxime Bourget <bmx007@gmail.com>
2012-02-13 13:19:21 +0000

    Add labellable to Tube

--- spec/labware/tube_spec.rb
+++ spec/labware/tube_spec.rb
@@ -2,6 +2,7 @@
 require 'labware/spec_helper'
 require 'labware/located_examples'
 require 'labware/receptacle_examples'
+require 'labware/labellable_examples'
 
 # Model requirements
 require 'lims/core/labware/tube'
@@ -10,5 +11,6 @@ module Lims::Core::Labware
   describe Tube  do
     it_behaves_like "located" 
     it_behaves_like "receptacle"
+    it_behaves_like "labellable"
   end
 end
