82545c91ccf2fd93995df4985a05ed42f06fb7ca
Maxime Bourget <bmx007@gmail.com>
2012-03-20 17:46:46 +0000

    Remove Quota.

--- lib/lims-core/organization/project.rb
+++ lib/lims-core/organization/project.rb
@@ -4,7 +4,7 @@ require 'common'
 module Lims::Core
   module Organization
     # A project corresponds to a source of funding.
-    # It has a manager and {Quota quotas}.
+    # It has a manager.
     class Project
     end
   end
