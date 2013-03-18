10f9d45ade26bbc6184ed4d950837ee813925fbc
max <bmx007@gmail.com>
2013-03-18 15:46:20 +0000

    Rename persistor

--- rename.rb
+++ rename.rb
@@ -28,20 +28,27 @@ resource_reg = /#{directory_map.keys.map { |d| "\\b#{d}" }.join('|')}/
 old_module = nil
 new_module = nil
 new_class = nil
+old_class = nil
 resource = nil
 resource_class = nil
 ARGF.each do |line|
   case line
-  when  /^(filename.*)\/(actions|persistence)\/((#{resource_reg}).*).rb/
-    resource = $4
+  when  /^(filename.*)\/(actions|persistence)\/(\w\+\/)?((#{resource_reg}).*).rb/
+    resource = $5
     if new_directory=directory_map[resource.to_sym]
       old_module = $2.capitalize
       current_directory = new_directory
       new_module = current_directory.to_s.capitalize
-      new_class = $3.split('_').map(&:capitalize).join
+      filename =$4
+      if $2 == "persistence"
+        filename += "_#{$3}" if $3
+        filename += "_persistor"
+      end
+      new_class = filename.split('_').map(&:capitalize).join
+      old_class = $4.split('_').map(&:capitalize).join
       resource_class = resource.split('_').map(&:capitalize).join
       
-      puts "#{$1}/#{current_directory}/#{resource}/#{$3}.rb"
+      puts "#{$1}/#{current_directory}/#{resource}/#{filename}.rb"
     else # normal file
       old_module = nil
       current_directory = nil
@@ -49,12 +56,14 @@ ARGF.each do |line|
     end
   when /module\s+#{old_module || '<>'}/
       puts line.sub(old_module, new_module)
-  when /(class|module)\s+#{new_class || '<>'}/
-      puts line.sub(new_class, "#{resource_class}::#{new_class}")
+  when /(class|module)\s+#{old_class || '<>'}/
+      puts line.sub(old_class, "#{resource_class}::#{new_class}")
   when  /^(\s+require.*)\/(actions|persistence)\/((#{resource_reg}).*)/
-    puts ">> #{line}"
     resource = $4
     if new_directory=directory_map[resource.to_sym]
+      if $2 == "persistence"
+        resource += "_persistor"
+      end
       puts "#{$1}/#{new_directory}/#{$resource}/#{$3}"
     else
       puts line
